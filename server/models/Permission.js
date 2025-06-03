5be6b262bdee3a019048af2acb37c00c32c5736c
Vinith1801 <vinithbylapudi@gmail.com>
2025-06-03 11:41:31 +0530

    added permissions, roles and roles with permissions files

--- server/models/Permission.js
+++ server/models/Permission.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose");
 
-const permissionSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-    },
+const permissionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  description: String
+});
 
-const Permission = mongoose.model("Permission", permissionSchema);
-module.exports = Permission;
+module.exports = mongoose.model("Permission", permissionSchema);
